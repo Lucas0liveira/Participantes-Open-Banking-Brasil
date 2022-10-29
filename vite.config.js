398f6f233c76302e98f4717895a379f95a37bebe
lucas0liveira <lucas0liveira96@outlook.com>
2022-10-29 10:12:55 -0400

    chore: changes vue version

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { createVuePlugin } from "vite-plugin-vue2";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [createVuePlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
