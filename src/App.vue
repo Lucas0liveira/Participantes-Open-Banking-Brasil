6a3879cbd89244596caafc8491769147db70083b
lucas0liveira <lucas0liveira96@outlook.com>
2022-10-29 15:53:13 -0400

    feat: setting up lazy-load

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,14 @@
 </template>
 
 <script>
-import { fetchFromSandbox } from "@/services/participants.service";
-
 export default {
   name: "App",
-  data: () => ({
-    loading: false,
-    data: [],
-  }),
-  async mounted() {
+
+  mounted() {
     this.$router.push("/dashboard");
   },
-  methods: {
-    async fetchDataAsync() {
-      try {
-        this.loading = true;
-        const organisations = await fetchFromSandbox();
-        this.data = organisations[0];
-      } catch (error) {
-        console.error(error);
-      } finally {
-        this.loading = false;
-      }
-    },
-  },
+
+  methods: {},
 };
 </script>
 
